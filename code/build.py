89552e8f5d24e8cc19af3cd1efb3ebf45877cf8b
pabloSanchezBB <t.rowland711@gmail.com>
2022-01-06 17:24:24 -0600

    made a script to run the build.py file

---
+++ code/build.py
@@ -0,0 +1,17 @@
+# This is the main Program for connecting to the TDA API
+from tda import auth, client
+import json
+
+import config as cf
+import functions as f
+
+# Main Program:
+"""
+try:
+	c = auth.client_from_token_file(cf.token_path, cf.api_key)
+except FileNotFoundError:
+	from selenium import webdriver
+	with webdriver.Firefox(executable_path = r'/opt/homebrew/bin/geckodriver') as driver:
+		c = auth.client_from_login_flow(driver, cf.api_key, cf.redirect_uri, cf.token_path)
+"""
+print("test worked!")
---
+++ code/functions.py
@@ -0,0 +1,5 @@
+# This will contain Function Definitions for Connecting to the TDA API
+import config as cf
+import json
+from tda import auth, client
+
